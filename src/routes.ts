c378b18ecb716917b02d7c06bf158064f0ceb82a
Ellen Gavronski <ellengavronski.el@hotmail.com>
2024-07-05 18:27:19 -0300

    Buscando por cursos

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
-import { coursesController } from './controllers/courseController'
+import { coursesController } from './controllers/coursesController'
 
 const router = express.Router()
 
@@ -9,6 +9,7 @@ router.get('/categories/id:', categoriesController.show)
 
 router.get('/courses/featured', coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
+router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
 export { router }
